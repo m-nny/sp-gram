77879184ee30e7ba3632b81a0e813b58392f8241
Alibek Manabayev <m-nny@yandex.ru>
2021-09-12 09:02:48 +0600

    [init] admin guard

--- apps/tbot/src/app/app.update.ts
+++ apps/tbot/src/app/app.update.ts
@@ -1,3 +1,4 @@
+import { UseFilters, UseGuards } from '@nestjs/common';
 import {
   Command,
   Help,
@@ -9,28 +10,36 @@ import {
 } from 'nestjs-telegraf';
 import { Telegraf } from 'telegraf';
 import { TgContext } from './common/context.interface';
+import { TelegrafExceptionFilter } from './common/filters/telegraf-exception.filter';
+import { AdminGuard } from './common/guards/admin.guard';
 import { ReverseTextPipe } from './common/pipes/reverse-text.pipe';
 
 @Update()
+@UseFilters(TelegrafExceptionFilter)
 export class AppUpdate {
   constructor(@InjectBot() private bot: Telegraf<TgContext>) {}
+
   @Start()
   async onStart() {
     const me = await this.bot.telegram.getMe();
     return `Hey, I'm ${me.first_name}`;
   }
+
   @Help()
   async onHelp() {
     return `Send me any text`;
   }
+
   @Command('admin')
+  @UseGuards(AdminGuard)
   onAdminCommand(): string {
     return `Welcome admin`;
   }
+
   @On('text')
   onMessage(
-    @Message('text', new ReverseTextPipe()) originalText: string
+    @Message('text', new ReverseTextPipe()) reversedText: string
   ): string {
-    return originalText;
+    return reversedText;
   }
 }
---
+++ apps/tbot/src/app/common/guards/admin.guard.ts
@@ -0,0 +1,19 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { TelegrafException, TelegrafExecutionContext } from 'nestjs-telegraf';
+import { TgContext } from '../context.interface';
+
+@Injectable()
+export class AdminGuard implements CanActivate {
+  private readonly ADMIN_IDS = [];
+
+  canActivate(context: ExecutionContext): boolean {
+    const ctx = TelegrafExecutionContext.create(context);
+    const { from } = ctx.getContext<TgContext>();
+
+    const isAdmin = this.ADMIN_IDS.includes(from.id);
+    if (!isAdmin) {
+      throw new TelegrafException(`You're not admin`);
+    }
+    return true;
+  }
+}
